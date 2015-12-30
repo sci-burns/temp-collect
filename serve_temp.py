db286bc7d392b808a68c4758cd1da4481fb8db85
DavidBurns <david.burns@pega.com>
2015-12-30 17:55:39 -0500

    read from file

--- serve_temp.py
+++ serve_temp.py
@@ -1,18 +1,34 @@
 #!/usr/bin/python
 from flask import Flask, jsonify
 import gviz_api
-import datetime
+from datetime import *
+from time import *
+
+fileName = "test.csv"
 
 app = Flask(__name__)
 
 table = {"time":   ("datetime", "Time"),
          "temp":   ("number",   "Temp")}
 
-data = [{"time": datetime.datetime(2015,12,30,10,0,0,0), "temp": (66.1, "66.1 F")},
-        {"time": datetime.datetime(2015,12,30,10,1,0,0), "temp": (68.3, "68.3 F")}]
+data = []
+
+def readLines():
+    myFile = open(fileName, 'r')
+    lines = myFile.readlines()
+    myFile.close()
+
+    for line in lines:
+        entry = {}
+        fields = line.rstrip().split(',')
+        theTime = strptime(fields[0].rstrip(), "%Y-%m-%d %H:%M:%S")
+        entry["time"] = datetime.fromtimestamp(mktime(theTime))
+        entry["temp"] = (float(fields[1]), fields[1].rstrip() + " F")
+        data.append(entry)
 
 @app.route('/')
 def index():
+    readLines()
     data_table = gviz_api.DataTable(table)
     data_table.LoadData(data)
     resp = app.make_response(data_table.ToJSon(columns_order=("time", "temp"),order_by="time"))
