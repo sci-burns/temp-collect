291473d5e3100bf6db0edf08c65ed9f849c8f5d6
DavidBurns <david.burns@pega.com>
2015-12-30 21:15:38 -0500

    fix warnings

--- serve_temp.py
+++ serve_temp.py
@@ -1,40 +1,60 @@
 #!/usr/bin/python
-from flask import Flask, jsonify
+from flask import Flask
 import gviz_api
 from datetime import *
 from time import *
+import time
 
 fileName = "test.csv"
 
 app = Flask(__name__)
 
-table = {"time":   ("datetime", "Time"),
-         "temp":   ("number",   "Temp")}
+table = {"time": ("datetime", "Time"),
+         "temp": ("number", "Temp")}
 
 data = []
 
-def readLines():
-    data = []
-    myFile = open(fileName, 'r')
-    lines = myFile.readlines()
-    myFile.close()
+
+def readlines():
+    try:
+        myfile = open(fileName, 'r')
+        lines = myfile.readlines()
+        myfile.close()
+        print "clean read " + str(len(lines))
+    except:
+        print "Exception?"
+        lines = "except"
+
+    return lines
+
+
+def scrapedata():
+    del data[:]
+    lines = readlines()
+
+    if lines == "except":
+        print "sleeping for 5 second...."
+        time.sleep(5)
+        lines = readlines()
 
     for line in lines:
         entry = {}
         fields = line.rstrip().split(',')
-        theTime = strptime(fields[0].rstrip(), "%Y-%m-%d %H:%M:%S")
-        entry["time"] = datetime.fromtimestamp(mktime(theTime))
+        thetime = strptime(fields[0].rstrip(), "%Y-%m-%d %H:%M:%S")
+        entry["time"] = datetime.fromtimestamp(mktime(thetime))
         entry["temp"] = (float(fields[1]), fields[1].rstrip() + " F")
         data.append(entry)
 
+
 @app.route('/')
 def index():
-    readLines()
+    scrapedata()
     data_table = gviz_api.DataTable(table)
     data_table.LoadData(data)
-    resp = app.make_response(data_table.ToJSon(columns_order=("time", "temp"),order_by="time"))
+    resp = app.make_response(data_table.ToJSon(columns_order=("time", "temp"), order_by="time"))
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True)
+    app.run(host='0.0.0.0', debug=True)
